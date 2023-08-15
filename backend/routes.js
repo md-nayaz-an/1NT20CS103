6bf89eab334d15f052de5165de808406ed57d577
Mohammed Nayaz <mohammednayazan@gmail.com>
2023-08-15 11:31:53 +0530

    api fetch working

---
+++ backend/routes.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const routes = express.Router();
+
+const url = "http://20.244.56.144/train/"
+let token = "";
+const authData = {
+    "companyName": "N",
+    "clientID": "67e0a0f9-f5ef-43e2-b1ac-6861c5743fe7",
+    "clientSecret": "sEzrVkbaIRyaNUvC",
+    "ownerName": "Ram",
+    "ownerEmail": "ram@abc.edu",
+    "rollNo": "1NT20"
+}
+
+async function auth () {
+    try {
+        const response = await fetch(url + "auth", {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(authData)
+        });
+    
+        if (!response.ok) {
+          throw new Error(`Authentication request failed with status: ${response.status}`);
+        }
+    
+        const responseBody = await response.json();
+        token = responseBody.access_token; 
+      } catch (error) {
+        console.error('Authentication error:', error.message);
+      }
+}
+
+routes.route("/train/trains").get(
+    async(req, res) => {
+        try {
+            await auth();
+            const response = await fetch(url + "/trains", {
+              method: 'GET',
+              headers: {
+                'Authorization': `Bearer ${token}`
+              }
+            });
+        
+            if (!response.ok) {
+              throw new Error(`Request failed with status: ${response.status}`);
+            }
+        
+            const data = await response.json();
+            res.json(data);
+          } catch (error) {
+            console.error('Error fetching data:', error.message);
+            res.status(500).json({ error: 'An error occurred while fetching data' });
+          }
+    }
+)
+
+module.exports = routes
--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ app.use(cors());
 
 app.use(express.json());
 
-//app.use(require('./routes'));
+app.use(require('./routes'));
 
 app.listen(port, () => {
 
