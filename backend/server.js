55d25c29132fc38a194dfec49f7211825cb0bbfc
Mohammed Nayaz <mohammednayazan@gmail.com>
2023-08-15 10:31:11 +0530

    server initialized

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = 8000;
+
+app.use(cors());
+
+app.use(express.json());
+
+//app.use(require('./routes'));
+
+app.listen(port, () => {
+
+  console.log(`Server started at port: ${port}`);
+})
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
